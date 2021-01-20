166f14a7dfbb461585212aac5f5c69ecdc3eaf58
Lydia Cho <lydiacho@gse.havard.edu>
2021-01-20 19:51:05 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,33 @@
+var chocolateBars = [
+  'snickers', 
+  'hundred grand',
+  'kitkat',
+  'skittles'
+  ];
+
+
+var addElementToBeginningOfArray = (array, element) => {
+ return addElementToBeginningOfArray;
+};
+ addElementToBeginningOfArray.unshift(1);
+
+
+var destructivelyAddElementToBeginningOfArray = (array, element) => {
+  array.unshift(element);
+  return array;
+};
+
+var accessElementInArray = (array, index) => {
+  
+};
+
+var destructivelyRemoveElementFromBeginningOfArray = () => {
+  destructivelyRemoveElementFromBeginningOfArray.shift();
+  return destructivelyRemoveElementFromBeginningOfArray;
+};
+
+var removeElementFromBeginningOfArray = () => {
+  
+  return removeElementFromBeginningOfArray;
+};
+removeElementFromBeginningOfArray.slice(1);
