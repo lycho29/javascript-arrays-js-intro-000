8805a4ad5225e52f721c2652dadef7649fe83225
Lydia Cho <lydiacho@gse.havard.edu>
2021-01-20 20:35:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -16,7 +16,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray (array, element) {
-  return array;
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray (array, element) {
