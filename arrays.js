df72f2e6cbac454a0fc73fca84c4e51fe212b3c3
Lydia Cho <lydiacho@gse.havard.edu>
2021-01-20 20:41:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,7 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift();
+  array.shift(element);
   return array;
 }
 
