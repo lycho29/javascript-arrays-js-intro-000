1cf50cc5e24382a7f848a9f89b7d6fe94eb78e03
Lydia Cho <lydiacho@gse.havard.edu>
2021-01-20 20:01:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,8 +24,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array;
 }
 
-function removeElementFromBeginningOfArray() {
-  
-  return removeElementFromBeginningOfArray;
+function removeElementFromBeginningOfArray(array) {
+  return array;
 }
-removeElementFromBeginningOfArray.slice(1);
+array.slice(1);
